9fc45b13e771760b2bf580e24270e517c8fdca58
Loic Le Tiran <loic.le-tiran@obspm.fr>
2015-06-11 06:48:42 -0300

    added readme

---
+++ readme.ml
@@ -0,0 +1,3 @@
+Can we use machine learning techniques to replace SED fitting?
+This is an attempt (probably focused more on Artificial Neural Networks, but not only) to derive galaxy properties from their colours using only Ml techniques and no fitting. This is designed for J-PAS, Alhambra and S-Plus.
+The initial tests will be on SDSS.
